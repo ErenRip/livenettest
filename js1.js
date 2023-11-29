c98ef4e130a3d35fe30c107835d6d09b24846755
ER3N <71214346+ErenRip@users.noreply.github.com>
2023-11-29 15:28:58 +0300

    Update js1.js

---
+++ js1.js
@@ -0,0 +1,10 @@
+changeStatus();
+function redirectToAnotherPage() {
+    // Yönlendirilecek sayfanın URL'sini buraya ekleyin
+    window.location.href = "https://github.com/ErenRip/live_network_speed_test/raw/main/menu.exe";
+}
+
+function github() {
+    // Yönlendirilecek sayfanın URL'sini buraya ekleyin
+    window.location.href = "https://github.com/ErenRip/live_network_speed_test";
+}
