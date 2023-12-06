ec1ba3ac846281c191e57e67ac5d51424859a4b3
ER3N <71214346+ErenRip@users.noreply.github.com>
2023-12-06 19:07:36 +0300

    Update titleana.js

--- titleana.js
+++ titleana.js
@@ -1,11 +1,20 @@
 
 
 var default1 = "Anasayfa"; // Genel başlık
-var text1 = "Anas ayfa"; // 1. Yazı
-var text2 = "Anasayfa"; // 2. Yazı
-var text3 = "Anas ayfa"; // 3. Yazı
-var text4 = "Anasayfa"; // 4. Yazı
-var text5="Anas ayfa";
+var text1 = "Anasayf"; // 1. Yazı
+var text2 = "Anasay"; // 2. Yazı
+var text3 = "Anasa"; // 3. Yazı
+var text4 = "Anas"; // 4. Yazı
+var text5="Ana";
+var text6="An";
+var text7="A";
+var text8="An";
+var text9 = "Ana"; 
+var text10 = "Anas"; 
+var text11 = "Anasa"; 
+var text12 = "Anasay"; 
+var text13 = "Anasayf";
+var text14 = "Anasayfa";  
 var changeRate = 450; // Zaman ayarı [1000 = 1 Saniye]
 var messageNumber = 0;
 
@@ -33,15 +42,50 @@ else if (messageNumber == 4) {
 else if (messageNumber == 5) {
     window.status=text5;
     document.title=text5;
+}
+else if (messageNumber == 6) {
+    window.status=text6;
+    document.title=text6;
+}
+else if (messageNumber == 7) {
+    window.status=text7;
+    document.title=text7;
+}
+else if (messageNumber == 8) {
+    window.status=text8;
+    document.title=text8;
+}
+else if (messageNumber == 9) {
+    window.status=text9;
+    document.title=text9;
+}
+else if (messageNumber == 10) {
+    window.status=text10;
+    document.title=text10;
+}
+else if (messageNumber == 11) {
+    window.status=text11;
+    document.title=text11;
+}
+else if (messageNumber == 12) {
+    window.status=text12;
+    document.title=text12;
+    
+}
+else if (messageNumber == 13) {
+    window.status=text13;
+    document.title=text13;
+}
+else if (messageNumber == 14) {
+    window.status=text14;
+    document.title=text14;
     messageNumber = 0;
 }
-
-
 messageNumber++;
 setTimeout("changeStatus();",changeRate);
 
 }
-
+ 
 
 
 changeStatus();
