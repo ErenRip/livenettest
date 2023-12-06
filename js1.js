7ba82a5f107639384cc0aaf2bd3b720aa940cdd8
ER3N <71214346+ErenRip@users.noreply.github.com>
2023-12-06 19:08:36 +0300

    Update js1.js

--- js1.js
+++ js1.js
@@ -8,3 +8,20 @@ function github() {
     // Yönlendirilecek sayfanın URL'sini buraya ekleyin
     window.location.href = "https://github.com/ErenRip/live_network_speed_test";
 }
+
+function githubp() {
+    // Yönlendirilecek sayfanın URL'sini buraya ekleyin
+    window.location.href = "https://github.com/ErenRip/";
+}
+function youtube() {
+    // Yönlendirilecek sayfanın URL'sini buraya ekleyin
+    window.location.href = "https://www.youtube.com/channel/UCoWYlEPKmZLrrNc_GYUeViQ";
+}
+function discord() {
+    // Yönlendirilecek sayfanın URL'sini buraya ekleyin
+    window.location.href = "https://discord.gg/r8eKnMJnME";
+}
+function blogger() {
+    // Yönlendirilecek sayfanın URL'sini buraya ekleyin
+    window.location.href = "https://codehubcommunity.blogspot.com";
+} 
